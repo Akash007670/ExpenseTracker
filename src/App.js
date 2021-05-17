ceea45c7280cd56d7303ef6652c076a05e31685c
Akash Kumar <akaysharma894@gmail.com>
2021-05-18 00:19:30 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
   const expenses = [
     {title : "Car Insurance" , amount : 2369 , date : new Date(2,2,2001)},
     {title : "College Fees" , amount : 65000 , date : new Date(3,3,2003)},
-    {title : "House Rent" , amount : 5000 , date : new Date(4,4,2004)}
+    {title : "House Rent" , amount : 5000 , date : new Date(4,4,2004)},
+    {title : "Groceries " , amount : 3000 , date : new Date(4,4,2004)}
   ];
   return (
     <div className="App">
