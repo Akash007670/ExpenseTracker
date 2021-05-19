90491071a72fb6a3b477aa67a15e2f3a2444d20c
Akash Kumar <akaysharma894@gmail.com>
2021-05-19 10:46:57 +0530

    updated expense handler

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -9,8 +10,15 @@ function App() {
     {title : "House Rent" , amount : 5000 , date : new Date(4,4,2004)},
     {title : "Groceries " , amount : 3000 , date : new Date(4,4,2004)}
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  }
+
   return (
     <div className="App">
+      <NewExpense onAddExpense = {addExpenseHandler}/>
         <Expenses items = {expenses}/>
     </div>
   );
