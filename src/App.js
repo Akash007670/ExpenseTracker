c63e5bbee76a72863bfcab5a3c36747055a0e24b
Akash Kumar <akaysharma894@gmail.com>
2021-05-23 23:45:03 +0530

    updated expense handler

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
-  const expenses = [
-    {title : "Car Insurance" , amount : 2369 , date : new Date(2,2,2001)},
-    {title : "College Fees" , amount : 65000 , date : new Date(3,3,2003)},
-    {title : "House Rent" , amount : 5000 , date : new Date(4,4,2004)},
-    {title : "Groceries " , amount : 3000 , date : new Date(4,4,2004)}
-  ];
+  const dummy_expenses = [];
+
+  const [expenses , setExpenses ] = useState(dummy_expenses);
 
   const addExpenseHandler = (expense) => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense ,...prevExpenses];
+    })
   }
 
   return (
