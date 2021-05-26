f7f69c4e9d0e59369da2c1ea12888ecb9a45008c
Akash Kumar <akaysharma894@gmail.com>
2021-05-26 23:36:21 +0530

    Added expense list

---
+++ src/components/Expenses/ExpensesList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./ExpensesList.css";
+import ExpenseItem from "./ExpenseItem";
+
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className="expense-list-fallback">Found no expenses.</h2>;
+  }
+  return (
+    <li>
+      <ul className="expense-list">
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </ul>
+    </li>
+  );
+};
+
+export default ExpensesList;
